f02a6f2a3e7c23f0fee46e97a584ba5561623f16
aurudin <renon@renon.com.br>
2020-03-24 16:30:27 -0300

    Formulario

--- js/app.js
+++ js/app.js
@@ -83,3 +83,16 @@ $(window).ready(function () {
         jQuery(".topBar").toggle();
     });
 });
+
+$(document).ready(function(){
+    var behavior = function (val) {
+        return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+    },
+    options = {
+        onKeyPress: function (val, e, field, options) {
+            field.mask(behavior.apply({}, arguments), options);
+        }
+    };
+    
+    $('#tel').mask(behavior, options);
+});
